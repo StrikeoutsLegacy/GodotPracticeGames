fba0067c0db0ea28c71bf5c7672f3fb68e7017a3
Dylan Carpenter <149751956+StrikeoutsLegacy@users.noreply.github.com>
2023-11-05 00:50:08 -0500

    Basic Player Script

---
+++ player.gd
@@ -0,0 +1,73 @@
+extends CharacterBody2D
+
+
+
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+const speed = 80
+var current_dir = "none"
+
+func _physics_process(_delta):
+	player_movement(_delta)
+
+func player_movement(_delta):
+	
+	if Input.is_action_pressed("right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("down"):
+		current_dir = "down"
+		play_anim(1)
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("up"):
+		current_dir = "up"
+		play_anim(1)
+		velocity.y = -speed
+		velocity.x = 0
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
+	
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+	if dir == "down":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("front_walk")
+		elif movement == 0:
+			anim.play("front_idle")
+	if dir == "up":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("up_walk")
+		elif movement == 0:
+			anim.play("up_idle")
+
+func player():
+	pass
